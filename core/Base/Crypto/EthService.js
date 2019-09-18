5a3208a345043f66fa7700988759bc061fcc6ede
Jie Xiao <jie.xiao@gtdollar.com>
2019-09-18 16:48:31 +0800

    fix bug

--- core/Base/Crypto/EthService.js
+++ core/Base/Crypto/EthService.js
@@ -27,27 +27,30 @@ class EthService {
     async getTransaction(txid) {
         return this.web3.eth.getTransaction(txid);
     }
-    async getBalance(address){
+
+    async getBalance(address) {
         return this.web3.eth.getBalance(address);
     }
-    async getGasPrice(){
+
+    async getGasPrice() {
         return this.web3.eth.getGasPrice();
     }
-    async sendSignedTransaction(fromAddress,toAddress,tradeAmount,privateKey){
+
+    async sendSignedTransaction(fromAddress, toAddress, tradeAmount, privateKey) {
         let gasLimit = 21000;
         let gasLimitHex = this.web3.utils.toHex(gasLimit);
-        let amountHex =  this.web3.utils.toHex(this.web3.utils.toWei(tradeAmount.toString(), 'ether'));
+        let amountHex = this.web3.utils.toHex(this.web3.utils.toWei(tradeAmount.toString(), 'ether'));
         let gasPrice = await this.web3.eth.getGasPrice();
         let gasPriceHex = this.web3.utils.toHex(gasPrice);
         let account = this.web3.eth.accounts.privateKeyToAccount(privateKey);
         let nonce = await this.web3.eth.getTransactionCount(fromAddress);
-        let nonceHex =  this.web3.utils.toHex(nonce);
+        let nonceHex = this.web3.utils.toHex(nonce);
         privateKey = privateKey.startsWith('0x') ? privateKey.substring(2) : privateKey;
         let privateKeyHex = new Buffer(privateKey, 'hex');
         let rawTx = {
             // chainId:1234,
-            nonce:nonceHex,
-            from:fromAddress,
+            nonce: nonceHex,
+            from: fromAddress,
             to: toAddress,
             value: amountHex,
             gasLimit: gasLimitHex,
@@ -56,109 +59,114 @@ class EthService {
         let tx = new EthereumTx(rawTx);
         tx.sign(privateKeyHex);
         let serializedTx = tx.serialize();
-        let txObj= await this.web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
+        let txObj = await this.web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
         console.log(txObj);
         return txObj
     }
-    weiToEther(weiVal){
-        return this.web3.utils.fromWei(weiVal.toString(),'ether');
+
+    weiToEther(weiVal) {
+        return this.web3.utils.fromWei(weiVal.toString(), 'ether');
     }
-    hexToNumber(hexVal){
+
+    hexToNumber(hexVal) {
         return this.web3.utils.toBN(this.web3.utils.hexToNumberString(hexVal));
     }
-    isAddress(address){
+
+    isAddress(address) {
         return this.web3.utils.isAddress(address);
     }
+
     //ERC20 Token
-    async getTokenBalance(blockAddress,contractAddress){
+    async getTokenBalance(blockAddress, contractAddress) {
         var web3 = this.web3;
         let contractABI = [{
             "constant": true,
             "inputs": [
-              {
-                "name": "_owner",
-                "type": "address"
-              }
+                {
+                    "name": "_owner",
+                    "type": "address"
+                }
             ],
             "name": "balanceOf",
             "outputs": [
-              {
-                "name": "balance",
-                "type": "uint256"
-              }
+                {
+                    "name": "balance",
+                    "type": "uint256"
+                }
             ],
             "payable": false,
             "type": "function"
         },
-        {
-            "constant": true,
-            "inputs": [
-              {
-                "name": "_to",
-                "type": "address"
-              },
-              {
-                "name": "_value",
-                "type": "uint256"
-              }
-            ],
-            "name": "transfer",
-            "outputs": [
-              {
-                "name": "success",
-                "type": "bool"
-              }
-            ],
-            "payable": false,
-            "type": "function"
-        }];
-        let tokenContract = new web3.eth.Contract(contractABI,contractAddress);
+            {
+                "constant": true,
+                "inputs": [
+                    {
+                        "name": "_to",
+                        "type": "address"
+                    },
+                    {
+                        "name": "_value",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "transfer",
+                "outputs": [
+                    {
+                        "name": "success",
+                        "type": "bool"
+                    }
+                ],
+                "payable": false,
+                "type": "function"
+            }];
+        let tokenContract = new web3.eth.Contract(contractABI, contractAddress);
         return tokenContract.methods.balanceOf(blockAddress).call();
     }
-    async getTokenEstimateGas(toAddress,tradeAmount,privateKey,contractAddress,tokenDecimals){
+
+    async getTokenEstimateGas(toAddress, tradeAmount, privateKey, contractAddress, tokenDecimals) {
         var web3 = this.web3;
         let contractABI = [{
             "constant": true,
             "inputs": [
-              {
-                "name": "_owner",
-                "type": "address"
-              }
+                {
+                    "name": "_owner",
+                    "type": "address"
+                }
             ],
             "name": "balanceOf",
             "outputs": [
-              {
-                "name": "balance",
-                "type": "uint256"
-              }
+                {
+                    "name": "balance",
+                    "type": "uint256"
+                }
             ],
             "payable": false,
             "type": "function"
         },
-        {
-            "constant": true,
-            "inputs": [
-              {
-                "name": "_to",
-                "type": "address"
-              },
-              {
-                "name": "_value",
-                "type": "uint256"
-              }
-            ],
-            "name": "transfer",
-            "outputs": [
-              {
-                "name": "success",
-                "type": "bool"
-              }
-            ],
-            "payable": false,
-            "type": "function"
-        }];
-        let tokenContract = new web3.eth.Contract(contractABI,contractAddress);
-        let amountHex =  this.web3.utils.toHex(this.web3.utils.toWei(tradeAmount.toString(), 'ether'));
+            {
+                "constant": true,
+                "inputs": [
+                    {
+                        "name": "_to",
+                        "type": "address"
+                    },
+                    {
+                        "name": "_value",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "transfer",
+                "outputs": [
+                    {
+                        "name": "success",
+                        "type": "bool"
+                    }
+                ],
+                "payable": false,
+                "type": "function"
+            }];
+        let tokenContract = new web3.eth.Contract(contractABI, contractAddress);
+        let amountHex = this.web3.utils.toHex(this.web3.utils.toWei(tradeAmount.toString(), 'ether'));
         let transferABI = tokenContract.methods.transfer(toAddress, amountHex).encodeABI();
         let gasPrice = await web3.eth.getGasPrice();
         let gasPriceHex = web3.utils.toHex(web3.utils.toBN(gasPrice));
@@ -171,7 +179,7 @@ class EthService {
             to: toAddress,
             value: '0x0',
             gasPrice: gasPriceHex,
-            gasLimit:await web3.utils.toHex(210000),
+            gasLimit: await web3.utils.toHex(210000),
             from: fromAddress,
             nonce: nonceHex,
             data: transferABI
@@ -179,7 +187,7 @@ class EthService {
         return web3.eth.estimateGas(rawTx);
     }
 
-    async sendTokenSignedTransaction(fromAddress,toAddress, tradeAmount, privateKey, contractAddress, tokenDecimals,gas_to_use=21000) {
+    async sendTokenSignedTransaction(fromAddress, toAddress, tradeAmount, privateKey, contractAddress, tokenDecimals, gas_to_use = 21000) {
         var web3 = this.web3;
         let contractABI = [{
             "constant": true,
@@ -222,8 +230,8 @@ class EthService {
                 "type": "function"
             }];
         // let account = web3.eth.accounts.privateKeyToAccount(privateKey);
-        let tokenContract = new web3.eth.Contract(contractABI, contractAddress,{from: fromAddress});
-        let amountHex = this.web3.utils.toHex(tradeAmount* 10**tokenDecimals );
+        let tokenContract = new web3.eth.Contract(contractABI, contractAddress, {from: fromAddress});
+        let amountHex = this.web3.utils.toHex(parseInt(tradeAmount * 10 ** tokenDecimals));
         let transferABI = tokenContract.methods.transfer(toAddress, amountHex).encodeABI();
         let gasPrice = await web3.eth.getGasPrice();
         let gasPriceHex = web3.utils.toHex(gasPrice);
@@ -235,23 +243,22 @@ class EthService {
             to: contractAddress,
             value: '0x0',
             gasPrice: gasPriceHex,
-            from:fromAddress,
-            nonce:nonceHex,
-            gasLimit:await web3.utils.toHex(210000),
+            from: fromAddress,
+            nonce: nonceHex,
+            gasLimit: await web3.utils.toHex(210000),
 
             data: transferABI
         };
         let tx = new EthereumTx(rawTx);
         tx.sign(privateKeyHex);
         let serializedTx = tx.serialize();
-        let txObj=  await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
+        let txObj = await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'));
         console.log(txObj);
         return txObj
     }
 }
 
 module.exports = EthService;
-
 
 
 // let a= new EthService('https://mainnet.infura.io/v3/9552ac202e2c4dfb9f1a986b71d86d4a');
