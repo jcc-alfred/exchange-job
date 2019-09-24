2b30075ae0a18b5dc445d379c5d039137e6ce4b9
Jie Xiao <jie.xiao@gtdollar.com>
2019-09-24 11:10:18 +0800

    add aim and usdt for report

--- core/Base/Utils/CryptoUtils.js
+++ core/Base/Utils/CryptoUtils.js
@@ -19,8 +19,9 @@ let CryptoUtils = {
             return '';
         }
     }
-}
+};
 
 module.exports = CryptoUtils;
 
 
+// console.log(CryptoUtils.aesDecode('20774f623169515d75322ca087a2f72ef8343d94920d585f93f8b29a49637f04ae5dca13f930e48d9e1d40b585ba3999dca3928129bb3397ee7c092b422683179006f5e48062f30436889d106f3086c0'))
