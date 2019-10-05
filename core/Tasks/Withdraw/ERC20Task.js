895265092ce8d6d6b32911544d14efb716d285be
Jie Xiao <jie.xiao@gtdollar.com>
2019-10-06 05:12:10 +0800

    update

--- core/Tasks/Withdraw/ERC20Task.js
+++ core/Tasks/Withdraw/ERC20Task.js
@@ -28,7 +28,7 @@ try{
     rule.minute = times;
 
     let isRun = false;
-    var job = schedule.scheduleJob(rule,async()=>{
+    var job = schedule.scheduleJob('1 * * * * *',async()=>{
         
         if(isRun){
             return;
